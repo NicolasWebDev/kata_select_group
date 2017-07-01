80316725e2bab83be5e19f284569dd20b7cd3f8f
Sathors <sathors@free.fr>
2017-07-01 14:11:41 -0500

    The backend function is implemented.

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -4,6 +4,59 @@ angular.module('Group')
 .controller('group', function ($scope) {
 
   $scope.controller_loaded = 'Group loaded!';
+  $scope.pairs = [];
+  $scope.pair = [];
+
+  $scope.addPair = function(pair) {
+    $scope.pairs.push(pair);
+    $scope.pair = [];
+  };
+
+  $scope.choose_employees_going = function(pairs) {
+    var sorted_employees = $scope.ids_sorted_by_nb_pairs(pairs);
+    var result = [];
+    var remaining_pairs = pairs;
+    while (remaining_pairs.length) {
+      var current_employee = sorted_employees.pop();
+      result.push(current_employee);
+      remaining_pairs = $scope.remove_pairs_with_employee(
+        remaining_pairs, current_employee
+      );
+    }
+    return result;
+  };
+
+  $scope.remove_pairs_with_employee = function(pairs, employee_id) {
+    return pairs.filter(function (pair) {
+      return pair[0] !== employee_id && pair[1] !== employee_id;
+    });
+  };
+
+  $scope.flatten_object = function(object) {
+    return Object.keys(object).map(function (property) {
+      return [property, object[property]];
+    });
+  };
+
+  $scope.ids_sorted_by_nb_pairs = function(pairs) {
+    return $scope.flatten_object($scope.nb_pairs_by_id(pairs))
+      .sort(function (first_pair, second_pair) {
+        return first_pair[1] - second_pair[1];
+      }).map(function (mapping) {
+        return Number(mapping[0]);
+      });
+  };
+
+  $scope.nb_pairs_by_id = function(pairs) {
+    var mapping = {};
+    pairs.forEach(function (pair) {
+      var first = pair[0];
+      var second = pair[1];
+      mapping[first] = mapping[first] === undefined ? 1 : mapping[first] + 1;
+      mapping[second] = mapping[second] === undefined ? 1 : mapping[second] + 1;
+    });
+    return mapping;
+  };
 
 })
 .config(function ($routeProvider) {
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -6,15 +6,75 @@ describe('Controller: select group', function () {
 
   var controller;
   var scope;
+  var pairs1;
+  var pairs2;
+  var pairs3;
 
   beforeEach(inject(function ($rootScope, $controller) {
     scope = $rootScope.$new();
     controller = $controller('group', { $scope: scope });
+    pairs1 = [[1009, 2011], [1017, 2011]];
+    pairs2 = [[1009, 2000], [1009, 2001], [1002, 2002], [1003, 2002]];
+    pairs3 = [[1, 2], [1, 3], [1, 6], [1, 7], [4, 7], [2, 3], [2, 4]];
   }));
 
   describe('On instance', function () {
+    it('adds a pair to the scope pairs', function() {
+      expect(scope.pairs).toEqual([]);
+      scope.addPair([1, 2]);
+      expect(scope.pairs).toEqual([[1, 2]]);
+      scope.addPair([3, 4]);
+      expect(scope.pairs).toEqual([[1, 2], [3, 4]]);
+    });
     it('should set "controller_loaded" variable in scope', function () {
       expect(scope.controller_loaded).toContain('loaded');
+    });
+
+    it('chooses the employess that go to the beach', function() {
+      expect(scope.choose_employees_going(pairs1)).toEqual([2011]);
+    });
+
+    it('chooses the employess that go to the beach case 2', function() {
+      expect(scope.choose_employees_going(pairs2)).toEqual([2002, 1009]);
+    });
+
+    it('chooses the employess that go to the beach case 3', function() {
+      expect(scope.choose_employees_going(pairs3)).toEqual([1, 2, 7]);
+    });
+
+    it('removes the pairs which contain the given employee', function() {
+      expect(scope.remove_pairs_with_employee(pairs1, 2011)).toEqual([]);
+      expect(scope.remove_pairs_with_employee(pairs1, 1009)).toEqual([[1017, 2011]]);
+      expect(scope.remove_pairs_with_employee(pairs1, 1017)).toEqual([[1009, 2011]]);
+    });
+
+    it('returns the ids of the employees ordered by their number of pairs', function () {
+      expect(scope.ids_sorted_by_nb_pairs(pairs1)).toEqual([1009, 1017, 2011]);
+    });
+
+    it('returns the ids of the employees ordered by their number of pairs case 2', function () {
+      expect(scope.ids_sorted_by_nb_pairs(pairs2)).toEqual(
+        [1002, 1003, 2000, 2001, 1009, 2002]
+      );
+    });
+
+    it('returns a mapping of ids/number_of_pairs', function() {
+      expect(scope.nb_pairs_by_id(pairs1)).toEqual({
+        1009: 1,
+        1017: 1,
+        2011: 2,
+      });
+    });
+
+    it('returns a mapping of ids/number_of_pairs case 2', function() {
+      expect(scope.nb_pairs_by_id(pairs2)).toEqual({
+        1009: 2,
+        1002: 1,
+        1003: 1,
+        2000: 1,
+        2001: 1,
+        2002: 2,
+      });
     });
   });
 
